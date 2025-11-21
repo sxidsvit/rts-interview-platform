fb093b71214bd14583977dddf7c7af452461f39a
Sergiy Antonyuk <s.p.antonyuk@gmail.com>
2025-11-21 23:15:11 +0100

    frontendBuildPath

--- backend/src/server.js
+++ backend/src/server.js
@@ -25,7 +25,7 @@ const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const frontendBuildPath = path.join(__dirname, '..', '..', 'frontend', 'dist');
+const frontendBuildPath = path.join(__dirname, '..', '..', 'frontend', 'build');
 
 app.use(express.json());
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
